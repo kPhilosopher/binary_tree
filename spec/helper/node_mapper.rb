68af9b238da45bf927db39047ca3ae35c393f5ea
Jinwoo Baek <jbaek@thoughtworks.com>
2014-02-15 00:05:07 -0800

    Adding capability to go deeper into the tree for the node mapper.

--- spec/helper/node_mapper.rb
+++ spec/helper/node_mapper.rb
@@ -3,16 +3,16 @@ require_relative '../../src/node'
 class NodeMapper < Node
   attr_accessor :right_node, :left_node, :value
 
-  def map
-    string_to_return = ''
+  def map(string_to_return=nil)
+    string_to_return = '' if string_to_return == nil
     if @left_node != nil
-      string_to_return << @left_node.value.to_s 
+      string_to_return << @left_node.map()
       string_to_return << ', '
     end
     string_to_return << @value.to_s
     if @right_node != nil
       string_to_return << ', '
-      string_to_return << @right_node.value.to_s 
+      string_to_return << right_node.map()
     end
     return string_to_return
   end
--- spec/helper/node_mapper_spec.rb
+++ spec/helper/node_mapper_spec.rb
@@ -5,42 +5,52 @@ describe NodeMapper, '#map' do
   let(:root) { NodeMapper.new }
 
   it 'maps the one node' do
-    @root = NodeMapper.new
-    @root.value = 1
+    root = NodeMapper.new
+    root.value = 1
     
-    output = @root.map()
+    output = root.map()
 
-    output.should eq("1")
+    output.should eq('1')
   end
 
   it 'maps the left, middle, then the right node' do
-    @root = NodeMapper.new
-    @root.value = 1
-    @root.left_node = NodeMapper.new
-    @root.left_node.value = 0
-    @root.right_node = NodeMapper.new
-    @root.right_node.value= 2
+    root = NodeMapper.new
+    root.value = 1
+    root.left_node = NodeMapper.new
+    root.left_node.value = 0
+    root.right_node = NodeMapper.new
+    root.right_node.value= 2
 
-    output = @root.map()
+    output = root.map()
 
-    output.should eq("0, 1, 2")
+    output.should eq('0, 1, 2')
   end
 
-  #it 'maps the left, middle, then the right node' do
-  #  @root = NodeMapper.new
-  #  @root.value = 3
-  #  @root.left_node = NodeMapper.new
-  #  @root.left_node.value = 1
-  #  @root.left_node.left_node = NodeMapper.new
-  #  @root.left_node.left_node.value = 0
-  #  @root.left_node.right_node = NodeMapper.new
-  #  @root.left_node.right_node.value = 2
-  #  @root.right_node = NodeMapper.new
-  #  @root.right_node.value= 4
+  it 'maps the left, middle, then the right node' do
+    root = NodeMapper.new
+    root.value = 30 
+    root.left_node = NodeMapper.new
+    root.left_node.value = 1
+    root.left_node.left_node = NodeMapper.new
+    root.left_node.left_node.value = 0
+    root.left_node.right_node = NodeMapper.new
+    root.left_node.right_node.value = 2
+    root.left_node.right_node.right_node = NodeMapper.new
+    root.left_node.right_node.right_node.value = 9
+    root.left_node.right_node.right_node.left_node = NodeMapper.new
+    root.left_node.right_node.right_node.left_node.value = 8
+    root.left_node.right_node.right_node.right_node = NodeMapper.new
+    root.left_node.right_node.right_node.right_node.value = 10
+    root.right_node = NodeMapper.new
+    root.right_node.value= 40
+    root.right_node.right_node = NodeMapper.new
+    root.right_node.right_node.value = 100
+    root.right_node.right_node.right_node = NodeMapper.new
+    root.right_node.right_node.right_node.value = 1000
 
-  #  output = @root.map()
+    output = root.map()
 
-  #  output.should eq("0, 1, 2, 3, 4")
-  #end
+    output.should eq('0, 1, 2, 8, 9, 10, 30, 40, 100, 1000')
+  end
 end
 
