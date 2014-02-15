c1037ee0c79e6112f4e9aeb1bbc987296201a375
Jinwoo Baek <jbaek@thoughtworks.com>
2014-02-15 00:05:24 -0800

    Refactoring tests.

--- spec/binary_search_tree_spec.rb
+++ spec/binary_search_tree_spec.rb
@@ -1,44 +1,32 @@
 require_relative '../src/binary_search_tree'
 
 describe BinarySearchTree, '#insert' do
-  it 'adds a node to a binary search tree' do
-    binarySearchTree = BinarySearchTree.new(Fixnum)
-    binarySearchTree.insert(1)['success'].should eq(true)
-    binarySearchTree.search(1).value.should eq(1)
-  end
+  let(:binary_search_tree) { BinarySearchTree.new(Fixnum) }
 
-  it 'adds two nodes to a binary search tree' do
-    binarySearchTree = BinarySearchTree.new(Fixnum)
-    binarySearchTree.insert(1)['success'].should eq(true)
-    binarySearchTree.insert(2)['success'].should eq(true)
-    binarySearchTree.search(1).value.should eq(1)
-    binarySearchTree.search(2).value.should eq(2)
- end
-  
+  it 'adds a node to a binary search tree' do
+    binary_search_tree.insert(1)['success'].should eq(true)
+    binary_search_tree.search(1).value.should eq(1)
+  end
+ 
   it 'adds three nodes to a binary search tree' do
-    binarySearchTree = BinarySearchTree.new(Fixnum)
-    binarySearchTree.insert(2)['success'].should eq(true)
-    binarySearchTree.insert(1)['success'].should eq(true)
-    binarySearchTree.insert(3)['success'].should eq(true) 
-    binarySearchTree.search(3).value.should eq(3)
-    binarySearchTree.search(2).value.should eq(2)
-    binarySearchTree.search(1).value.should eq(1)
+    binary_search_tree.insert(2)['success'].should eq(true)
+    binary_search_tree.insert(1)['success'].should eq(true)
+    binary_search_tree.insert(3)['success'].should eq(true) 
+    binary_search_tree.search(3).value.should eq(3)
+    binary_search_tree.search(2).value.should eq(2)
+    binary_search_tree.search(1).value.should eq(1)
   end
 
   it 'should only allow acceptable types' do
-    binarySearchTree = BinarySearchTree.new(Fixnum)
-    returnValue = binarySearchTree.insert('someString')
-    returnValue['success'].should eq(false)
-    returnValue['errorMessage'].should eq('Invalid input value type')
+    return_value = binary_search_tree.insert('someString')
+    return_value['success'].should eq(false)
+    return_value['errorMessage'].should eq('Invalid input value type')
   end
   
-  it 'should return map with success equal to false' do
-    binarySearchTree = BinarySearchTree.new(Fixnum)
-    binarySearchTree.insert(3)['success'].should eq(true) 
-    returnValue = binarySearchTree.insert(3)
-    returnValue['success'].should eq(false)
-    returnValue['errorMessage'].should eq('Element already exists')
+  it 'should return map with success equal to false when inserting multiple times' do
+    binary_search_tree.insert(3)['success'].should eq(true) 
+    return_value = binary_search_tree.insert(3)
+    return_value['success'].should eq(false)
+    return_value['errorMessage'].should eq('Element already exists')
   end
-
-  
 end
